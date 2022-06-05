cac7e8df7f84e506448157c7ed4c5ea30b1fa41f
Draqode <Boomjay008@gmail.com>
2022-06-05 16:17:44 +0100

    feat: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,15 @@
+import { EmailIcon } from '@chakra-ui/icons'
+import { Box, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <Flex justifyContent='space-between' alignItems='flex-start' px={['', '', '40px', '60px', '100px']}>
+      <Box>
+        <EmailIcon fontSize='56px' />
+        <Text> DraqBureau  produces innovative and essential investigative journalism, opinions</Text>
+      </Box>
+      <Box></Box>
+    </Flex>
   )
 }
