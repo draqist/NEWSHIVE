bc9c42ed3afac88a572b8acdbe990797e3c0a390
Draqode <Boomjay008@gmail.com>
2022-06-21 22:49:24 +0100

    fix:likely security breach of access

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
 module.exports = {
   nextConfig,
   env: {
-    customKey: 'pub_8560f55193a7e949af97c16b150d171bf5ad',
-    oldApi: 'pub_8468f717acdbf7f18873914d92fd1e757d3d',
-    abdullahApi: 'pub_846859bdc626f105b50914d23b67c1b05a76'
+    customKey: process.env.customKey,
+    oldApi: process.env.oldApi,
+    abdullahApi: process.env.abdullahApi
   },
 } 
