29a6ff4dbc04d91ba14d9be50a4d8e33487b9139
Draqode <Boomjay008@gmail.com>
2022-06-29 22:35:19 +0100

    fix: removed env redundancy

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
 module.exports = {
   nextConfig,
   env: {
-    maemoonah: process.env.maemoonah,
     oldApi: process.env.oldApi,
     abdullahApi: process.env.abdullahApi,
     maemoonah: process.env.ckey,
